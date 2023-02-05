2a8a94ac79edfbbeda78f812a142013bc721483c
slordian <arthur.silva.2@academico.ifpb.edu.br>
2023-02-05 16:02:40 -0300

    todas as classes implementadasmas o programa ainda não roda exatamente

--- forma.py
+++ forma.py
@@ -1,3 +1,5 @@
+from math import pi, sqrt
+
 class Forma():
     def __init__(self, lado1, lado2):
         """Construtor"""
@@ -6,27 +8,57 @@ class Forma():
     
     def calc_area(self):
         """Calcula a área da figura que dá nome à classe"""
-        return self.lado1 * self.lado2
+        return float(self.lado1 * self.lado2)
     
     def __str__(self):
-        """Modifica o que vai ser impresso com print"""
-        return f'A área do {str(self.__class__.__name__).lower()} é de {self.calc_area()}.' 
+        """Modifica o que vai ser impresso"""
+        return f'A área do {str(self.__class__.__name__).lower()} é de {self.calc_area():.3f}.' 
     
 class Retangulo(Forma):
     pass
 
 class Quadrado(Retangulo):
-    def __init__(lado):
+    def __init__(self, lado):
         """Igual a Retangulo, mas passa o argumento lado duas vezes para a função __init__ do Retangulo, pois todos os seus lados são iguais"""
         super().__init__(lado, lado)
-
-# Tem algum problema com a chamada de calc_area do triangulo, resolve issaí        
+      
 class Triangulo(Retangulo):
     def __init__(self, base, altura):
-        """Igual o do Retangulo mudando apenas o nome dos lados, pois, no caso do Triangulo, os lados tem nomes diferentes"""
+        """Igual o do Retangulo mudando apenas o nome dos lados, pois, 
+        no caso do Triangulo, os lados tem nomes diferentes"""
         super().__init__(base, altura)
     
-    def calc_area():
-          return super().calc_area()
+    def calc_area(self):
+          return super().calc_area() / 2
+      
+class Circulo(Forma):
+    def __init__(self, raio):
+        self.raio = raio
+
+    def calc_area(self):
+        return pi * self.raio ** 2
     
-breakpoint() 
+class Hexagono(Quadrado):
+    def calc_area(self):
+        area = (3 * (self.lado1 ** 2) * sqrt(3)) / 2
+        return area
+
+class Programa():
+    def __init__(self):
+        pass
+    
+    # implementa isso aí depois kkkkk
+    def run():
+        while True:
+            pass
+    
+    
+    
+def main():
+    run = Programa()
+    run.run()
+
+    
+if __name__ == '__main__':
+    main()
+    
